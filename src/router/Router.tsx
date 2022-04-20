18888e3fd516e6c90410a7e2ef3e19288f2e6f95
valflrt <vfleurit@gmail.com>
2022-04-20 19:31:39 +0200

    edit Router.tsx

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Main from "../routes/Main";
+import Editor from "../routes/Editor";
 import NotFound from "../routes/NotFound";
 
 const Router = () => (
   <>
     <Routes>
       <Route path={"/"} element={<Main />} />
+      <Route path={"/editor"} element={<Editor />} />
       <Route path={"*"} element={<NotFound />} />
     </Routes>
   </>
