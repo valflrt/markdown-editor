9bc89a19cf3fa9b446a37337955679b1558111ef
valflrt <vfleurit@gmail.com>
2022-04-23 16:49:14 +0200

    edit Editor/index.tsx - adds two spaces when tab is pressed in the textarea (except if shift is pressed at the same time) - add titles

--- src/routes/Editor/index.tsx
+++ src/routes/Editor/index.tsx
@@ -1,42 +1,64 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 
 import useMarkdown from "../../hooks/useMarkdown";
-
-/* import ReactSyntaxHighlighter from "react-syntax-highlighter";
-import mdStyle from "react-syntax-highlighter/dist/esm/styles/hljs/github"; */
 
 import styles from "./Editor.module.css";
 import "github-markdown-css/github-markdown-light.css";
 
-/* <ReactSyntaxHighlighter
-            language={"markdown"}
-            style={mdStyle}
-            showLineNumbers={true}
-          >
-            {textAreaValue}
-          </ReactSyntaxHighlighter> */
+/*
+possible way to display the textarea with its content highlighted:
+
+import ReactSyntaxHighlighter from "react-syntax-highlighter";
+import editorStyle from "react-syntax-highlighter/dist/esm/styles/hljs/github";
+
+<ReactSyntaxHighlighter
+  language={"markdown"}
+  style={editorStyle}
+  showLineNumbers={true}
+>
+  {textAreaValue}
+</ReactSyntaxHighlighter>
+*/
 
 const Editor = () => {
   let [textAreaValue, setTextAreaValue] = useState("");
   let parsedMarkdown = useMarkdown(textAreaValue);
 
+  useEffect(() => {
+    console.log(textAreaValue);
+  }, [textAreaValue]);
+
   return (
     <div className={styles.editorWrapper}>
       <div className={styles.textZoneWrapper}>
-        <div className={styles.textZone}>
-          <textarea
-            className={styles.textZoneTextArea}
-            value={textAreaValue}
-            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-              setTextAreaValue(e.target.value);
-            }}
-            /* onKeyPress={(e) => {
-              console.log(e.key);
-            }} */
-          ></textarea>
-        </div>
+        <h3>Write your raw Markdown here:</h3>
+        <textarea
+          className={styles.textZoneTextArea}
+          value={textAreaValue}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+            setTextAreaValue(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            // adds two spaces when pressing "tab" (and shift key is not pressed)
+            if (!e.shiftKey && e.key === "Tab") {
+              e.preventDefault();
+              let [start, end] = [
+                e.currentTarget.selectionStart,
+                e.currentTarget.selectionEnd,
+              ];
+              setTextAreaValue(
+                textAreaValue
+                  .substring(0, start)
+                  .concat("  ")
+                  .concat(textAreaValue.substring(end, textAreaValue.length))
+              );
+              e.currentTarget.setRangeText("  ", start, end, "end");
+            }
+          }}
+        />
       </div>
       <div className={styles.mdZoneWrapper}>
+        <h3>The parsed and colored output is here:</h3>
         <div className={styles.mdZone.concat(" markdown-body")}>
           {parsedMarkdown}
         </div>
